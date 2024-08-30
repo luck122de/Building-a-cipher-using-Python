37e55bbdbe71ac84c428c4ce47628735690ad1bb
Ankit Rai <railucky390@gmail.com>
2024-08-30 21:12:17 +0530

    this code for caeser cipher

--- Building a Cipher.py
+++ Building a Cipher.py
@@ -1,28 +1,15 @@
 text = 'Hello Zaira'
-custom_key = 'python'
+shift = 3
 
-def vigenere(message, key):
-    key_index = 0
-    alphabet = 'abcdefghijklmnopqrstuvwxyz'
-    encrypted_text = ''
+alphabet = 'abcd'
+encrypted_text = ''
 
-    for char in message.lower():
-    
-        # Append space to the message
-        if char == ' ':
-            encrypted_text += char
-        else:        
-            # Find the right key character to encode
-            key_char = key[key_index % len(key)]
-            key_index += 1
-
-            # Define the offset and the encrypted letter
-            offset = alphabet.index(key_char)
-            index = alphabet.find(char)
-            new_index = (index + offset) % len(alphabet)
-            encrypted_text += alphabet[new_index]
-    
-    return encrypted_text
-
-encryption = vigenere(text, custom_key)
-print(encryption)
+for char in text.lower():
+    if char == ' ':
+        encrypted_text += char
+    else:
+        index = alphabet.find(char)
+        new_index = (index + shift) % len(alphabet)
+        encrypted_text += alphabet[new_index]
+print('plain text:', text)
+print('encrypted text:', encrypted_text)
